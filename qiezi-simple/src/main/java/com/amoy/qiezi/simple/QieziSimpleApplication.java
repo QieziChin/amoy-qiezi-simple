1ea6e6da08d41355b94a6eaf49ac4c226eea27d3
qiezi <qiezi.chin@gmail.com>
2024-08-09 15:15:27 +0800

    项目初始化

---
+++ qiezi-simple/src/main/java/com/amoy/qiezi/simple/QieziSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.amoy.qiezi.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QieziSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QieziSimpleApplication.class, args);
+    }
+
+}
